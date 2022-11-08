cd088a80bdc77d3fe361dc7539ee62783ed55083
bernie6401 <bernie6401@gmail.com>
2022-11-08 16:19:46 +0800

    try to implement question3

--- HW3/Programming/temp.py
+++ HW3/Programming/temp.py
@@ -4,7 +4,19 @@ import numpy as np
 from PIL import Image
 import torchvision.transforms as T
 import torch.nn as nn
+import random
+# from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
 
+SEED = 5566
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+torch.manual_seed(SEED)
+torch.cuda.manual_seed_all(SEED)
+random.seed(SEED)
+np.random.seed(SEED)
 
 class Dataset(data.Dataset):
     def __init__(self, data_path):
@@ -64,6 +76,30 @@ class Net(nn.Module):
         
         return latent_vec, x_res
 
+def clustering(model, device, loader, n_iter, reduced_method, reduced_dim, perplexity):
+    assert reduced_method in ['pca', 'tsne', None]
+    
+    model.eval()
+    latent_vec = torch.tensor([]).to(device, dtype=torch.float)
+    for idx, (image_aug, image) in enumerate(loader):
+        print("predict %d / %d" % (idx, len(loader)) , end='\r')
+        image = image.to(device, dtype=torch.float)
+        latent, r = model(image)
+        latent_vec = torch.cat((latent_vec, latent), dim=0)
+
+    latent_vec = latent_vec.cpu().detach().numpy()
+    
+    if reduced_method == 'tsne':
+        tsne = TSNE(n_components=reduced_dim, verbose=1, method='exact', perplexity=perplexity, n_iter=n_iter)
+        latent_vec = tsne.fit_transform(latent_vec)
+    elif reduced_method == 'pca':
+        pca = PCA(n_components=reduced_dim, copy=False, whiten=True, svd_solver='full')
+        latent_vec = pca.fit_transform(latent_vec)
+
+    # kmeans = KMeans(n_clusters=2, random_state=0, max_iter=n_iter).fit(latent_vec)
+    return latent_vec#kmeans.labels_
+
+
 NUM_EPOCH = 5
 BATCH_SIZE = 32
 LATENT_DIM = 32
@@ -75,50 +111,117 @@ lr = 5e-4
 ''' ********************************************* 
     Create image
 ********************************************* '''
-# DATA_PATH = './dataset/trainX.npy'
-# dataset = Dataset(DATA_PATH)
+'''
+DATA_PATH = './dataset/trainX.npy'
+dataset = Dataset(DATA_PATH)
 
-# # Random split
-# train_set_size = int(len(dataset) * 0.85)
-# valid_set_size = len(dataset) - train_set_size
-# train_set, valid_set = data.random_split(dataset, [train_set_size, valid_set_size])
+# Random split
+train_set_size = int(len(dataset) * 1)
+valid_set_size = len(dataset) - train_set_size
+train_set, valid_set = data.random_split(dataset, [train_set_size, valid_set_size])
 
-# # set data loader
-# train_loader = data.DataLoader(train_set, batch_size=1, num_workers=1, shuffle=True)
-# valid_loader = data.DataLoader(valid_set, batch_size=1, num_workers=1, shuffle=False)
-# use_gpu = torch.cuda.is_available()
-# device = torch.device("cuda" if use_gpu else "cpu")
-# transform = T.ToPILImage()
+# set data loader
+train_loader = data.DataLoader(train_set, batch_size=1, num_workers=1, shuffle=False)
+valid_loader = data.DataLoader(valid_set, batch_size=1, num_workers=1, shuffle=False)
+use_gpu = torch.cuda.is_available()
+device = torch.device("cuda" if use_gpu else "cpu")
+transform = T.ToPILImage()
 
-# if __name__ == '__main__':
-    # for i, (image_aug, image) in enumerate(train_loader):
-    #     image = torch.squeeze(image, 0)
-    #     img = transform(image)
-    #     img.save("./img/hwq2_" + str(i) + ".png")
-
+if __name__ == '__main__':
+    for i, (image_aug, image) in enumerate(train_loader):
+        image = torch.squeeze(image, 0)
+        img = transform(image)
+        img.save("./dataset/img/hwq2_" + str(i) + ".png")
+'''
 
 ''' ********************************************* 
     Report Qrestion 2
 ********************************************* '''
+'''Try to transform image to numpy'''
+'''
 if __name__ == '__main__':
-    DATA_PATH = './dataset/trainX.npy'
+    DATA_PATH = './img/hwq2_'
+    image1 = Image.open(DATA_PATH + '0.png')
+    image1 = np.asarray(image1)
+    image2 = Image.open(DATA_PATH + '1.png')
+    image2 = np.asarray(image2)
+    total_img = np.zeros((2, 32, 32, 3))
+    # total_img = np.append(total_img, image1)
+    # total_img = np.append(total_img, image2)
+    # print(total_img[0].size())
+    total_img[0] = image1
+    total_img[1] = image2
+    
+    # total_img1 = np.load('./dataset/trainX.npy')
+    # image3 = total_img1[0]
+    # image4 = total_img1[1]
+    np.save('./img_npy/hwq2', total_img)
+if __name__ == '__main__':
+    # total_img = np.load('./dataset/trainX.npy')
+    # img1 = total_img[0]
+    # img2 = total_img[1]
+    DATA_PATH = './img_npy/hwq2.npy'
     dataset = Dataset(DATA_PATH)
     use_gpu = torch.cuda.is_available()
     device = torch.device("cuda" if use_gpu else "cpu")
 
     # Random split
-    train_set_size = int(len(dataset) * 0.85)
+    train_set_size = int(len(dataset) * 1)
     valid_set_size = len(dataset) - train_set_size
     train_set, valid_set = data.random_split(dataset, [train_set_size, valid_set_size])
 
     # set data loader
-    train_loader = data.DataLoader(train_set, batch_size=BATCH_SIZE, num_workers=1, shuffle=True)
-    valid_loader = data.DataLoader(valid_set, batch_size=BATCH_SIZE, num_workers=1, shuffle=False)
+    train_loader = data.DataLoader(train_set, batch_size=1, num_workers=1, shuffle=False)
+    valid_loader = data.DataLoader(valid_set, batch_size=1, num_workers=1, shuffle=False)
 
     model = Net(latent_dim=LATENT_DIM).to(device)
-    print("Loading pretrained weights...", args.checkpoint)
-    checkpoint = torch.load(args.checkpoint)
+    checkpoint = './model/1102_1_origin/epoch9_0.0107.pth'
+    print("Loading pretrained weights...", checkpoint)
+    checkpoint = torch.load(checkpoint)
     model.load_state_dict(checkpoint["model_state_dict"], strict=False)
 
-    for image_aug, image in train:
+    transform = T.ToPILImage()
+    criterion = nn.MSELoss()
+    for i, (image_aug, image) in enumerate(train_loader):
+        image = image.to(device, dtype=torch.float)
+        image_aug = image_aug.to(device, dtype=torch.float)
         _, reconsturct = model(image_aug)
+        image = torch.squeeze(image, 0)
+        img = transform(image)
+        loss = criterion(reconsturct, image)
+        img.save("./img/hwq2_reconstruct_" + str(i) + ".png")
+        print(loss)
+'''
+
+
+''' ********************************************* 
+    Report Qrestion 3
+********************************************* '''
+if __name__ == '__main__':
+    DATA_PATH = './dataset/visualization_X.npy'
+    dataset = Dataset(DATA_PATH)
+    use_gpu = torch.cuda.is_available()
+    device = torch.device("cuda" if use_gpu else "cpu")
+
+    # Random split
+    test_loader = data.DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
+    model = Net(latent_dim=LATENT_DIM).to(device)
+    checkpoint = './model/original/epoch4.pth'
+    print("Loading pretrained weights...", checkpoint)
+    checkpoint = torch.load(checkpoint)
+    model.load_state_dict(checkpoint["model_state_dict"], strict=False)
+
+    predicted = clustering(model, device, test_loader, NUM_ITER, reduced_method=REDUCED_METHOD, reduced_dim=REDUCED_DIM, perplexity=15)
+    print(predicted)
+    print(len(predicted))
+    # transform = T.ToPILImage()
+    # criterion = nn.MSELoss()
+    # for i, (image_aug, image) in enumerate(train_loader):
+    #     image = image.to(device, dtype=torch.float)
+    #     image_aug = image_aug.to(device, dtype=torch.float)
+    #     _, reconsturct = model(image_aug)
+    #     image = torch.squeeze(image, 0)
+    #     img = transform(image)
+    #     loss = criterion(reconsturct, image)
+    #     img.save("./img/hwq2_reconstruct_" + str(i) + ".png")
+    #     print(loss)
